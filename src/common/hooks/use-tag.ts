c040647ceb577dcfd10dccae343c4f1885497997
Huy Nguyen <huynguyen@fetch.tech>
2022-10-31 00:24:47 +0700

    feat: add tag to add post

--- src/common/hooks/use-tag.ts
+++ src/common/hooks/use-tag.ts
@@ -1,4 +1,10 @@
-import { UseMutationOptions, useMutation } from "@tanstack/react-query";
+import {
+  UseMutationOptions,
+  useMutation,
+  UseQueryOptions,
+  useQuery,
+} from "@tanstack/react-query";
+import { useMemo } from "react";
 import { z } from "zod";
 import {
   AddSyllabusTagsPayload,
@@ -7,6 +13,62 @@ import {
   ZTagItem,
 } from "../types/tag.type";
 import useApiAuth from "./use-api";
+
+export function useTags(args?: {
+  keySearch?: string;
+  config?: UseQueryOptions<TagItem[], Error, TagItem[], Array<any>>;
+}) {
+  const apiAuth = useApiAuth();
+
+  const fetchTags = useMemo(
+    () =>
+      z
+        .function()
+        .args(z.string())
+        .implement(async (keySearch: string) => {
+          const { data } = await apiAuth.get(`/tags?keySearch=${keySearch}`);
+
+          return data.map((item: TagItem) => ZTagItem.parse(item));
+        }),
+    []
+  );
+
+  const getTagsQuery = useQuery(
+    ["get-tags-query", args?.keySearch],
+    () => fetchTags(args?.keySearch || ""),
+    args?.config
+  );
+
+  return getTagsQuery;
+}
+
+export function useSyllabusTagsByClassroom(args?: {
+  classroomId: string;
+  config?: UseQueryOptions<TagItem[], Error, TagItem[], Array<any>>;
+}) {
+  const apiAuth = useApiAuth();
+
+  const fetchSyllabusTagsByClassroom = useMemo(
+    () =>
+      z
+        .function()
+        .args(z.string())
+        .implement(async (classroomId: string) => {
+          const { data } = await apiAuth.get(`/tags/syllabus/${classroomId}`);
+
+          return data.map((item: TagItem) => ZTagItem.parse(item));
+        }),
+    []
+  );
+
+  const getSyllabusTagsByClassroomQuery = useQuery(
+    ["get-syllabus-tags-by-classroom", args?.classroomId],
+    () => fetchSyllabusTagsByClassroom(args?.classroomId || ""),
+    args?.config
+  );
+
+  return getSyllabusTagsByClassroomQuery;
+}
 
 export function useAddSyllabusTags(args?: {
   config?: UseMutationOptions<
@@ -31,4 +93,24 @@ export function useAddSyllabusTags(args?: {
   );
 
   return addSyllabusTagsMutation;
+}
+
+export function useAddTag(args?: {
+  config?: UseMutationOptions<TagItem, Error, string, Array<any>>;
+}) {
+  const apiAuth = useApiAuth();
+
+  const addTagMutation = useMutation(
+    z
+      .function()
+      .args(z.string())
+      .implement(async (tag: string) => {
+        const { data } = await apiAuth.post("/tags", { tag });
+
+        return ZTagItem.parse(data);
+      }),
+    args?.config
+  );
+
+  return addTagMutation;
 }
--- src/feat/classroom/modal-add-post.tsx
+++ src/feat/classroom/modal-add-post.tsx
@@ -1,6 +1,7 @@
 import { PrimaryButton, SecondaryButton } from "@/common/components/button";
 import {
   Box,
+  Button,
   Dialog,
   DialogActions,
   DialogContent,
@@ -10,10 +11,16 @@ import {
   Typography,
 } from "@mui/material";
 import CloseTwoToneIcon from "@mui/icons-material/CloseTwoTone";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import ArticleEditor from "@/common/components/article-editor";
 import { useRouter } from "next/router";
 import { PostItem } from "@/common/types/post.type";
+import Select from "react-select";
+import { useSyllabusTagsByClassroom, useTags } from "@/common/hooks/use-tag";
+import { TagItem } from "@/common/types/tag.type";
+import useDebounce, {
+  SEARCH_DEBOUNCE_TIMEOUT,
+} from "@/common/hooks/use-debounce";
 
 export interface ISubmitPost {
   title: string;
@@ -35,7 +42,76 @@ const ModalAddPost = (props: IProps) => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
+  const [queryTags, setQueryTags] = useState("");
+  const debounceKeySearch = useDebounce(queryTags, SEARCH_DEBOUNCE_TIMEOUT);
+
   const [isLoading, setIsLoading] = useState(true);
+
+  const { data: dataTags, isFetching: fetchingGetTags } = useTags({
+    keySearch: debounceKeySearch,
+  });
+
+  const { data: dataSyllabusTags } = useSyllabusTagsByClassroom({
+    classroomId,
+  });
+
+  const syllabusTagsOptions = useMemo(() => {
+    if (!dataSyllabusTags) return [];
+
+    return dataSyllabusTags.map((item: TagItem) => ({
+      value: item.id,
+      label: item.tag,
+      type: item.type,
+    }));
+  }, [dataSyllabusTags]);
+
+  const tagsOptions = useMemo(() => {
+    if (!dataTags) return [];
+
+    return dataTags.map((item: TagItem) => ({
+      value: item.id,
+      label: item.tag,
+      type: item.type,
+    }));
+  }, [dataTags]);
+
+  const customStyles = useMemo(
+    () => ({
+      multiValue: (styles: any, { data }: any) => {
+        if (data.type === "SYLLABUS") {
+          return {
+            ...styles,
+            backgroundColor: "rgba(0, 82, 204, 0.1)",
+          };
+        }
+        return styles;
+      },
+      multiValueLabel: (styles: any, { data }: any) => {
+        if (data.type === "SYLLABUS") {
+          return {
+            ...styles,
+            color: "rgb(0, 82, 204)",
+          };
+        }
+
+        return styles;
+      },
+      multiValueRemove: (styles: any, { data }: any) => {
+        if (data.type === "SYLLABUS") {
+          return {
+            ...styles,
+            color: "rgb(0, 82, 204)",
+            ":hover": {
+              backgroundColor: "rgb(0, 82, 204)",
+              color: "white",
+            },
+          };
+        }
+        return styles;
+      },
+    }),
+    []
+  );
 
   useEffect(() => {
     if (!data) {
@@ -83,7 +159,41 @@ const ModalAddPost = (props: IProps) => {
             fullWidth
           />
         </Box>
-        <Box sx={{ p: 1 }}>
+        <Box sx={{ p: 1, mb: 1 }}>
+          <Typography variant="body1" sx={{ mb: 1 }}>
+            Tags
+          </Typography>
+          <Select
+            closeMenuOnSelect={false}
+            isMulti
+            options={debounceKeySearch ? tagsOptions : syllabusTagsOptions}
+            styles={customStyles}
+            onInputChange={(value) => {
+              setQueryTags(value);
+            }}
+            noOptionsMessage={({ inputValue }) => {
+              // console.log("fetch", fetchingGetTags, inputValue);
+
+              if (fetchingGetTags || inputValue !== debounceKeySearch)
+                return <>Loading...</>;
+
+              return (
+                <Button
+                  onClick={() => {
+                    console.log("new tag", inputValue);
+                    // setTest((prev) => [
+                    //   ...prev,
+                    //   { value: inputValue, label: inputValue },
+                    // ]);
+                  }}
+                >
+                  Add new tag
+                </Button>
+              );
+            }}
+          />
+        </Box>
+        <Box sx={{ p: 1, mb: 1 }}>
           <Typography variant="body1" sx={{ mb: 1 }}>
             Description
           </Typography>
